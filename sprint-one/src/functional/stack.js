cb1456e7162ef3603300b4c65a81df680e75b8cc
Danny Harmon <dannyaharmon@gmail.com>
2020-08-30 03:34:34 -0700

    Complete functional Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var index = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.pop = function() {
+    if (index > 0) {
+      index--;
+      var result = storage[index];
+      delete storage[index];
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return index;
   };
 
   return someInstance;
