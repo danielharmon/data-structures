df0ad8feed0f110e1e325cf11fc8c0b1939c4f55
Danny Harmon <dannyaharmon@gmail.com>
2020-08-31 10:01:44 -0700

    Complete ES6 Stack and Queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,22 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.start = 0;
+    this.current = 0;
+  }
+  enqueue(value) {
+    this[this.current] = value;
+    this.current += 1;
+  }
+  dequeue() {
+    if (this.current > 0) {
+      var result = this[this.start];
+      delete this[this.start];
+      this.start += 1;
+      return result;
+    }
+  }
+  size() {
+    return (this.current - this.start) < 0 ? 0 : (this.current - this.start);
   }
 
 }
