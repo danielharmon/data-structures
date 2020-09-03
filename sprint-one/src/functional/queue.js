a17cfd814407ef603ee18417efa0b5be62d347b0
Jenny Hou <jhou2012@gmail.com>
2020-09-03 11:22:31 -0400

    function basic reqs

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (Object.keys(storage).length === 0) {
+      storage[0] = value;
+    } else {
+      storage[Object.keys(storage).length] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+    var first = storage[0];
+    for (var key in storage) {
+      storage[Number(key)] = storage[Number(key + 1)];
+    }
+    delete storage[Object.keys(storage).length - 1];
+    return first;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
