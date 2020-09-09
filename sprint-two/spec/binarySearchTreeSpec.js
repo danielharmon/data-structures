0869f2a1bc148de95aa82794c297b602d2ac5243
Danny Harmon <dannyaharmon@gmail.com>
2020-09-08 19:53:15 -0700

    Dunno

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -44,4 +44,14 @@ describe('binarySearchTree', function() {
     binarySearchTree.insert(7);
     expect(binarySearchTree.size()).to.equal(4);
   });
+
+  it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.breadthFirstLog(func);
+    expect(array).to.eql([5, 2, 7, 3]);
+  });
 });
